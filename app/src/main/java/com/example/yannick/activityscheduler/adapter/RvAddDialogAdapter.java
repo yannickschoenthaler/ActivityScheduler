a7ed0a395b2e92214b83a01df6d147d0a0b6a57b
Unknown <jonas.resch@tfobz.net>
2018-06-01 13:06:11 +0200

    Started fix (not finished)

--- app/src/main/java/com/example/yannick/activityscheduler/adapter/RvAddDialogAdapter.java
+++ app/src/main/java/com/example/yannick/activityscheduler/adapter/RvAddDialogAdapter.java
@@ -23,15 +23,54 @@ public class RvAddDialogAdapter extends RecyclerView.Adapter<RvAddDialogAdapter.
     private ArrayList<CustomActivity> activities;
     private String[] activityTypes;
 
+    private enum ViewType {
+        NORMAL(0), RINGTONE(1);
+
+        private int value;
+
+        ViewType(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
     public RvAddDialogAdapter(ArrayList<CustomActivity> activities) {
         this.activities = activities;
     }
 
     @Override
     public HeaderViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.rv_add_dialog_item, parent, false);
-        return new HeaderViewHolder(itemView);
+        View itemView = null;
+        HeaderViewHolder viewHolder = null;
+
+        if (viewType == ViewType.NORMAL.getValue()) {
+            itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.rv_add_dialog_item, parent, false);
+            viewHolder = new SimpleViewHolder(itemView);
+
+        } else if (viewType == ViewType.RINGTONE.getValue()) {
+            itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.rv_add_dialog_item_ringtone, parent, false);
+            viewHolder = new RingtoneViewHolder(itemView);
+        }
+
+        return viewHolder;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        ViewType ret = ViewType.NORMAL;
+
+        CustomActivity activity = activities.get(position);
+
+        if (activity.getType() == ActivityType.RINGTONE) {
+            ret = ViewType.RINGTONE;
+        }
+
+        return ret.getValue();
     }
 
     @Override
@@ -41,33 +80,50 @@ public class RvAddDialogAdapter extends RecyclerView.Adapter<RvAddDialogAdapter.
 
     @Override
     public void onBindViewHolder(final HeaderViewHolder holder, int position) {
-        final CustomActivity activity = activities.get(position);
+        CustomActivity activity = activities.get(position);
         Context context = holder.itemView.getContext();
         Resources resources = context.getResources();
+        ActivityType activityType = activity.getType();
+
+        int viewType = getItemViewType(position);
+
         activityTypes = resources.getStringArray(R.array.custom_activity_types);
 
-        holder.update(activity);
+        holder.iv_activity_icon.setImageResource(activityType.getIconId());
+        holder.tv_type.setText(activityTypes[activityType.getId()]);
 
-//        holder.cl_item.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                activity.setExpanded(!activity.isExpanded());
-//
-//                if (activity.isExpanded()) {
-//                    if (activity.getIntFromType() == ActivityTypes1.RINGTONE) {
-//                        holder.cl_detail_ringtone.setVisibility(View.VISIBLE);
-//                    } else {
-//                        holder.cl_detail_normal.setVisibility(View.VISIBLE);
-//                    }
-//
-//                    holder.ib_arrow.setImageResource(R.drawable.ic_keyboard_arrow_up_black_24dp);
-//                } else {
-//                    holder.cl_detail_normal.setVisibility(View.GONE);
-//                    holder.cl_detail_ringtone.setVisibility(View.GONE);
-//                    holder.ib_arrow.setImageResource(R.drawable.ic_keyboard_arrow_down_black_24dp);
-//                }
-//            }
-//        });
+        if (viewType == ViewType.NORMAL.getValue()) {
+
+            SimpleViewHolder simpleViewHolder = (SimpleViewHolder) holder;
+
+        } else if (viewType == ViewType.RINGTONE.getValue()) {
+
+            RingtoneViewHolder ringtoneViewHolder = (RingtoneViewHolder) holder;
+
+        }
+
+        //holder.update(activity);
+
+        /*holder.cl_item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activity.setExpanded(!activity.isExpanded());
+
+                if (activity.isExpanded()) {
+                    if (activity.getIntFromType() == ActivityTypes1.RINGTONE) {
+                        holder.cl_detail_ringtone.setVisibility(View.VISIBLE);
+                    } else {
+                        holder.cl_detail_normal.setVisibility(View.VISIBLE);
+                    }
+
+                    holder.ib_arrow.setImageResource(R.drawable.ic_keyboard_arrow_up_black_24dp);
+                } else {
+                    holder.cl_detail_normal.setVisibility(View.GONE);
+                    holder.cl_detail_ringtone.setVisibility(View.GONE);
+                    holder.ib_arrow.setImageResource(R.drawable.ic_keyboard_arrow_down_black_24dp);
+                }
+            }
+        });*/
 
 
     }
@@ -79,21 +135,21 @@ public class RvAddDialogAdapter extends RecyclerView.Adapter<RvAddDialogAdapter.
 
     class HeaderViewHolder extends RecyclerView.ViewHolder {
         private TextView tv_type;
-        private ImageView tv_activity_icon, ib_arrow;
+        private ImageView iv_activity_icon, ib_arrow;
 
         public HeaderViewHolder(View itemView) {
             super(itemView);
 
             tv_type = itemView.findViewById(R.id.tv_type);
-            tv_activity_icon = itemView.findViewById(R.id.iv_activity_icon);
+            iv_activity_icon = itemView.findViewById(R.id.iv_activity_icon);
             ib_arrow = itemView.findViewById(R.id.ib_arrow);
         }
 
-        public void update(CustomActivity activity) {
+        /*public void update(CustomActivity activity) {
             ActivityType type = activity.getType();
             tv_type.setText(activityTypes[type.getId()]);
-            tv_activity_icon.setImageResource(type.getIconId());
-        }
+            iv_activity_icon.setImageResource(type.getIconId());
+        }*/
     }
 
     class SimpleViewHolder extends HeaderViewHolder {
@@ -107,12 +163,12 @@ public class RvAddDialogAdapter extends RecyclerView.Adapter<RvAddDialogAdapter.
             sw_status = itemView.findViewById(R.id.sw_status);
         }
 
-        public void update(CustomActivity activity) {
+        /*public void update(CustomActivity activity) {
             super.update(activity);
             ActivityType type = activity.getType();
             tv_status.setText(itemView.getContext().getString(R.string.status_text, activityTypes[type.getId()]));
             // TODO sw_status.setChecked(activity);
-        }
+        }*/
     }
 
     class RingtoneViewHolder extends HeaderViewHolder {
@@ -126,10 +182,10 @@ public class RvAddDialogAdapter extends RecyclerView.Adapter<RvAddDialogAdapter.
             alarm = itemView.findViewById(R.id.alarmBar);
         }
 
-        @Override
+        /*@Override
         public void update(CustomActivity activity) {
             super.update(activity);
             // TODO set seek bars
-        }
+        }*/
     }
 }
