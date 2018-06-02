d51d085923388d8267cc0996bb5e8299533fb2ae
Unknown <jonas.resch@tfobz.net>
2018-06-02 12:54:53 +0200

    Added fromJSON and toJSON methods

--- app/src/main/java/com/example/yannick/activityscheduler/model/Card.java
+++ app/src/main/java/com/example/yannick/activityscheduler/model/Card.java
@@ -3,28 +3,64 @@ package com.example.yannick.activityscheduler.model;
 import android.graphics.Bitmap;
 import android.widget.ImageButton;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Card implements Serializable{
+public class Card implements Serializable {
     private String title;
     private boolean activated;
     private Bitmap picture;
     private ArrayList<CustomActivity> activities = new ArrayList<>();
 
-    public Card(){
+    public Card() {
 
     }
 
-    public Card(String title, boolean isActivated, Bitmap picture){
+    public Card(String title, boolean isActivated, Bitmap picture) {
         this.title = title;
         this.activated = isActivated;
         this.picture = picture;
     }
 
+    public static Card fromJSON(JSONObject object) throws JSONException {
+        Card ret = new Card();
+
+        ret.setActivated(object.getBoolean("activated"));
+        ret.setTitle(object.getString("title"));
+
+        JSONArray activityArray = object.getJSONArray("activities");
+
+        for (int i = 0; i < activityArray.length(); i++) {
+            JSONObject jsonObject = activityArray.getJSONObject(i);
+            ret.addActivity(CustomActivity.fromJSON(jsonObject));
+        }
+
+        return ret;
+    }
+
+    public JSONObject toJSON() throws JSONException {
+        JSONObject ret = new JSONObject();
+
+        ret.put("activated", activated);
+        ret.put("title", title);
+
+        JSONArray activityArray = new JSONArray();
+        for (CustomActivity customActivity : activities) {
+            activityArray.put(customActivity.toJSON());
+        }
+
+        ret.put("activities", activityArray);
+
+        return ret;
+    }
+
     @Override
     public String toString() {
-        return title +" " +activated;
+        return title + " " + activated;
     }
 
     public String getTitle() {
--- app/src/main/java/com/example/yannick/activityscheduler/model/CustomActivity.java
+++ app/src/main/java/com/example/yannick/activityscheduler/model/CustomActivity.java
@@ -2,6 +2,9 @@ package com.example.yannick.activityscheduler.model;
 
 import com.example.yannick.activityscheduler.ActivityType;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.Serializable;
 import java.util.Calendar;
 
@@ -11,6 +14,9 @@ public class CustomActivity implements Serializable {
     private boolean isExpanded = true;
     private Calendar time;
     private ActivityType type = BLUETOOTH;
+
+    public CustomActivity() {
+    }
 
     public CustomActivity(ActivityType type, Calendar time) {
         this.type = type;
@@ -41,4 +47,21 @@ public class CustomActivity implements Serializable {
         return isExpanded;
     }
 
+    public static CustomActivity fromJSON(JSONObject object) throws JSONException{
+        CustomActivity ret = new CustomActivity();
+
+        ret.setExpanded(object.getBoolean("expanded"));
+        ret.setType(ActivityType.valueOfId(object.getInt("type")));
+
+        return ret;
+    }
+
+    public JSONObject toJSON() throws JSONException {
+        JSONObject ret = new JSONObject();
+
+        ret.put("expanded", isExpanded);
+        ret.put("type", type.getId());
+
+        return ret;
+    }
 }
